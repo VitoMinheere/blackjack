c0cf6501d757dbd726bc17d251941787766874d0
vito minheere <vitominheere@gmail.com>
2019-11-11 19:09:24 +0100

    Add new lines to all print statements

--- print_statements.py
+++ print_statements.py
@@ -13,7 +13,7 @@ def print_player_hand_value(player, hand_value):
 
 
 def print_player_is_bust(hand_value):
-    print('Hand value is ' + str(hand_value) + '. You are bust!')
+    print("Hand value is ' + str(hand_value) + '. You are bust!" + "\n")
 
 
 def print_bank_action(action):
@@ -21,31 +21,31 @@ def print_bank_action(action):
 
 
 def print_bank_has_bust(split):
-    print("Bank has bust. Everybody gets " + str(split))
+    print("Bank has bust. Everybody gets " + str(split) + "\n")
 
 
 def print_winner(winner):
-    print(str(winner) + " is the winner!")
+    print(str(winner) + " is the winner!" + "\n")
 
 
 def print_multiple_winners(winners):
-    print(str(winners) + " are the winners!")
+    print(str(winners) + " are the winners!" + "\n")
 
 
 def print_amount_money_player_has(player, amount):
-    print(player + " has " + str(amount) + " left")
+    print(player + " has " + str(amount) + " left" + "\n")
 
 
 def print_player_has_no_money_left(player):
-    print(player + " has no money left")
+    print(player + " has no money left" + "\n")
 
 
 def print_all_players_are_bust():
-    print("Every player has bust! The bank wins")
+    print("Every player has bust! The bank wins" + "\n")
 
 
 def print_amount_money_on_table(amount_on_table):
-    print("Amount on table is " + str(amount_on_table))
+    print("Amount on table is " + str(amount_on_table) + "\n")
 
 
 def print_amount_money_for_player(player, amount):
--- user_input.py
+++ user_input.py
@@ -1,15 +1,15 @@
 def ask_player_what_amount_to_bet(player):
     while True:
         try:
-            player_bet = int(input("You have " + str(player.get_money_amount()) + ". How much would you like to bet?"))
+            player_bet = int(input("You have " + str(player.get_money_amount()) + ". How much would you like to bet?" + "\n"))
             if player_bet <= player.get_money_amount() and player_bet > 0:
                 break
             elif player_bet == 0:
-                print("You must be more than 0")
+                print("You must be more than 0" + "\n")
             else:
-                print("You can only bet up to " + str(player.get_money_amount()))
+                print("You can only bet up to " + str(player.get_money_amount() + "\n"))
         except ValueError:
-            print('Please enter a number')
+            print('Please enter a number' + "\n")
     return player_bet
 
 
@@ -18,7 +18,7 @@ def ask_player_for_next_action(player):
     question = "What would you like to do? [H]it or [S]tand"
     if player.hand.check_hand_for_matching_cards():
         question = "You have two of the same cards, you can split your hand in 2 separate hands. " \
-                   + question + " or [SP]lit"
+                   + question + " or [SP]lit" + "\n"
         matching_cards = True
     while True:
         try:
@@ -30,48 +30,48 @@ def ask_player_for_next_action(player):
             elif action == 'SP' and matching_cards:
                 return action
             elif matching_cards:
-                print("Please enter H, S or SP")
+                print("Please enter H, S or SP" + "\n")
             else:
-                print("Please enter H or S")
+                print("Please enter H or S" + "\n")
         except ValueError:
             if matching_cards:
-                print("Please enter H, S or SP")
+                print("Please enter H, S or SP" + "\n")
             else:
-                print("Please enter H or S")
+                print("Please enter H or S" + "\n")
 
 
 def ask_player_for_another_round():
     while True:
         try:
-            answer = input("Would you like to play another round? [Y]es or [N]o ?").upper()
+            answer = input("Would you like to play another round? [Y]es or [N]o ?" + "\n").upper()
             if answer == "Y":
                 return answer
             elif answer == "N":
                 break
         except ValueError:
-            print("Please enter Y or N")
+            print("Please enter Y or N" + "\n")
 
 
 def ask_for_amount_of_players():
     while True:
         try:
-            amount_of_players = int(input("How many players would you like on the table?"))
+            amount_of_players = int(input("How many players would you like on the table?" + "\n"))
             if amount_of_players >= 1:
                 return amount_of_players
             else:
-                print("Please enter a number higher then 0")
+                print("Please enter a number higher then 0" + "\n")
         except ValueError:
-            print('Please enter a number')
+            print("Please enter a number" + "\n")
 
 
 def ask_player_for_name():
     while True:
         try:
-            player_name = input("What is your name?")
+            player_name = input("What is your name?" + "\n")
             if len(player_name) > 2:
                 break
             else:
-                print("Please enter a longer name")
+                print("Please enter a longer name" + "\n")
         except ValueError:
             pass
     return player_name
@@ -81,12 +81,12 @@ def ask_player_to_change_ace():
     while True:
         try:
             answer = input(
-                "You have an ace! The value is 11 but you can change it to 1. Do you want to change the value to 1? [Y]es of [N]o").upper()
+                "You have an ace! The value is 11 but you can change it to 1. Do you want to change the value to 1? [Y]es of [N]o" + "\n").upper()
             if answer == "Y":
                 return True
             elif answer == "N":
                 return False
             else:
-                print("Please enter Y or N")
+                print("Please enter Y or N" + "\n")
         except ValueError:
-            print("Please enter Y or N")
+            print("Please enter Y or N" + "\n")
